8fe4aea558316dd756c94beddbb26d67cb6c4150
swachha saha <swachha@gmail.com>
2026-08-17 14:33:31 +0530

    uupdate

--- modules/resource_group/main.tf
+++ modules/resource_group/main.tf
@@ -1,19 +1,5 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=5.0.0"
-    }
-  }
-}
-
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
-}
-
-
-resource "azurerm_resource_group" "example" {
-  name     = "swachha_rg"
-  location = "West Europe"
+resource "azurerm_resource_group" "rgs" {
+  for_each = var.resource_groups
+  name     = each.value.name
+  location = each.value.location
 }
